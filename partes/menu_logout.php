9d392f4af50d506a3ffbbae755153e5d83657e94
Anderson Rodrigues <anderson_ar2004@hotmail.com>
2012-05-10 00:29:30 -0300

    tira campo instituicao

--- partes/menu_logout.php
+++ partes/menu_logout.php
@@ -2,19 +2,15 @@
 	<img src="/img/banner_logout.png" width=766 height=195 />
 	<div id="tela_login">
 		<form action="sistema.php" method="POST">
-			<input type="radio" name="referencia" value=1 checked>DireÁ„o/SE</input><br>
-			<input type="radio" disabled="disabled" name="referencia" value=2>Secret·ria</input><br>
+			<input type="radio" disabled="disabled" name="referencia" value=0>Mantenedora</input><br>
+			<input type="radio" name="referencia" value=1 checked>Dire√ß√£o/SE</input><br>
+			<input type="radio" disabled="disabled" name="referencia" value=2>Secret√°ria</input><br>
 			<input type="radio" disabled="disabled" name="referencia" value=4>Professores</input><br>
 			<input type="radio" disabled="disabled" name="referencia" value=3>Alunos</input><br>
-			<input type="radio" disabled="disabled" name="referencia" value=5>Respons·veis</input>
 			<div id="campos_login">
 				<table>
 					<tr>
-						<td>InstituiÁ„o:</td>
-						<td><input type="text" name="instituicao"></td>
-					</tr>
-					<tr>
-						<td>Usu·rio:</td>
+						<td>Usu√°rio:</td>
 						<td><input type="text" name="usuario"></td>
 					</tr>
 					<tr>
@@ -22,8 +18,9 @@
 						<td><input type="password" name="senha"></td>
 					</tr>
 					<tr>
-						<td><a style="color: red; font-size: 9;"><?if(isset($_SESSION['login']) && ($_SESSION['login'] == false)) echo "Login inv·lido!";?></a></td>
+						<td><a style="color: red; font-size: 9;"><?if(isset($_SESSION['login']) && ($_SESSION['login'] == false)) echo "Login inv√°lido!";?></a></td>
 						<td>
+							<br>
 							<center>
 								<input type="submit" value="OK" style="background-color: gray; width: 70px;"><input type="reset" value="Limpar" style="background-color: gray; width: 70px;">
 							</center>
